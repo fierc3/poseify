76a7bc36af53c2e88d384ed786402c792b492f21
Corsin Kirchhofer <cxultimate@gmail.com>
2023-03-30 18:39:48 +0200

    Update Estimation.cs

--- src/Core/Models/Estimation.cs
+++ src/Core/Models/Estimation.cs
@@ -5,7 +5,7 @@ using System.Text.Json.Serialization;
 public class Estimation
 {
     public string Guid { get; set; } = "";
-    public List<Tag> Tags { get; set; } = new List<Tag>();
+    public List<string> Tags { get; set; } = new List<string>();
     public string UploadingProfile { get; set; } = null;
     public DateTime UploadDate { get; set; } = DateTime.Now;
 
