bf540003b0e481b6ff10b2de9715d77430041a33
Corsin Kirchhofer <cxultimate@gmail.com>
2023-03-30 11:31:10 +0200

    Tag and Estimation class created

---
+++ src/Core/Models/Estimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+[Serializable]
+public class Estimation
+{
+    public string Guid { get; set; } = "";
+    public List<Tag> Tags { get; set; } = new List<Tag>();
+    public UserProfile UploadingProfile { get; set; } = null;
+    public DateTime UploadDate { get; set; } = DateTime.Now;
+
+}
+//    session.Advanced.Attachments.Store(estimation.Guid, videoName+"_estimation_result", estimationFile);
+//    session.Advanced.Attachments.Store(estimation.Guid, videoName+"_preview", previewFile);
---
+++ src/Core/Models/Tag.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+[Serializable]
+public class Tag
+{
+    public string InternalGuid { get; set; } = "";
+    public string Name { get; set; } = "";
+}
+
