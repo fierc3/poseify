eccd5476df181d3cc9fddcde5ddba3bdfbffaf6d
Corsin Kirchhofer <cxultimate@gmail.com>
2023-03-30 18:39:02 +0200

    write attachment test code

--- src/Core/Controllers/UserProfileController.cs
+++ src/Core/Controllers/UserProfileController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Raven.Client.Documents;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Backend.Controllers { 
 
@@ -29,15 +30,47 @@ namespace Backend.Controllers {
             using (var session = _store.OpenSession())
             {
                 // use linq or rql to load Userprofile
-                data= session.Query<UserProfile>().Where(x => x.InternalGuid == guid).FirstOrDefault();
+                data = session.Query<UserProfile>().Where(x => x.InternalGuid == guid).FirstOrDefault();
             }
 
-            if(data == null)
+            if (data == null)
             {
                 return NotFound();
             }
 
             return data;
+        }
+
+        [HttpPost(Name = "TestSavingAttachmentToDb")]
+        public ActionResult<Estimation> Post(string guid)
+        {
+            _logger.LogDebug("Testing saving attachment to DB");
+            Estimation? estimation = null;
+            UserProfile? usr = null;
+            // get session for raven db
+            using (var session = _store.OpenSession())
+            using (var file1 = System.IO.File.Open("..\\..\\..\\poseify\\Estimation_test\\Uploads\\test_input\\test_man.mp4.npz", FileMode.Open))
+            {
+                usr = session.Query<UserProfile>().Where(x => x.InternalGuid == guid).FirstOrDefault();
+                string estimation_id = "estimation/1";
+                estimation = new Estimation
+                {
+                    Guid = "Testimation1",
+                    Tags = new List<string>(),
+                    UploadingProfile = usr.InternalGuid,
+                    UploadDate = DateTime.Now
+                };
+                session.Store(estimation, estimation_id);
+                session.Advanced.Attachments.Store(estimation_id, "test_attach.npz", file1);
+                session.SaveChanges();
+            }
+
+            if (estimation == null)
+            {
+                return NotFound();
+            }
+
+            return estimation;
         }
     }
 }
--- src/Core/Models/Estimation.cs
+++ src/Core/Models/Estimation.cs
@@ -6,9 +6,7 @@ public class Estimation
 {
     public string Guid { get; set; } = "";
     public List<Tag> Tags { get; set; } = new List<Tag>();
-    public UserProfile UploadingProfile { get; set; } = null;
+    public string UploadingProfile { get; set; } = null;
     public DateTime UploadDate { get; set; } = DateTime.Now;
 
 }
-//    session.Advanced.Attachments.Store(estimation.Guid, videoName+"_estimation_result", estimationFile);
-//    session.Advanced.Attachments.Store(estimation.Guid, videoName+"_preview", previewFile);
