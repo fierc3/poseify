9686ebd37946414857f91a65a55e66ebbea4a1c3
MM <cxultimate@gmail.com>
2023-04-06 13:13:39 +0200

    Add Database structure and Database interaction
    Tag and Estimation for Database, Handlers for interaction

---
+++ src/Core/Models/Estimation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+[Serializable]
+public class Estimation
+{
+    public string InternalGuid { get; set; } = "";
+    public string DisplayName { get; set; } = "";
+    public IEnumerable<string> Tags { get; set; } = new List<string>();
+    public string UploadingProfile { get; set; } = null;
+    public DateTime UploadDate { get; set; } = DateTime.Now;
+}
