2f3b7879538606a303d87143d01d41027b2603fb
Mike / iznogoud / fierc3 <mpullen9@gmail.com>
2023-04-25 21:15:01 +0200

    Add estimation view dialog with download options

---
+++ src/Core/Controllers/AttachmentController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AttachmentController : ControllerBase
+    {
+
+        private readonly ILogger<AttachmentController> _logger;
+        private readonly IEstimationService _estimationService;
+        private readonly IConfiguration _configuration;
+
+        public AttachmentController(ILogger<AttachmentController> logger, IEstimationService estimationHandler, IConfiguration configuration)
+        {
+            _logger = logger;
+            _estimationService = estimationHandler;
+            _configuration = configuration;
+        }
+        
+        [ActionName("GetAttachment")]
+        [HttpGet(Name = "GetAttachment")]
+        public async Task<IActionResult> Get(string estimationId, AttachmentType attachmentType)
+        {
+            var attachmentFile = _estimationService.GetEstimationAttachment(estimationId, attachmentType);
+
+            if(attachmentFile == null)
+            {
+                return Problem("Couldn't load attachment");
+            }
+
+            var attachmentName = attachmentType == AttachmentType.Joints ? Constants.JOINTS_FILENAME : Constants.PREVIEW_FILENAME;
+            return File(attachmentFile, "application/octet-stream", attachmentName);
+        }
+    }
+}
--- src/Core/Services/IEstimationService.cs
+++ src/Core/Services/IEstimationService.cs
@@ -1,8 +1,10 @@
 ﻿
 using Core.Models;
+using Raven.Client.Documents.Attachments;
 
 public interface IEstimationService
 {
     public Estimation HandleUploadedFile(string userGuid, string directory, string fileName, string fileExtension, string displayName, IEnumerable<Tag>? tags);
     public IEnumerable<Estimation> GetAllUserEstimations(string userGuid);
+    public Stream? GetEstimationAttachment(string estimationid, AttachmentType attachmentType);
 }
