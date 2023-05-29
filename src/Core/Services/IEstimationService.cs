b781a0eaa5f557e4ff8a3d46a91bedfc0439200d
Mike / iznogoud / fierc3 <mpullen9@gmail.com>
2023-05-29 18:58:52 +0200

    Improve error handeling

--- src/Core/Services/IEstimationService.cs
+++ src/Core/Services/IEstimationService.cs
@@ -4,7 +4,7 @@ using Raven.Client.Documents.Attachments;
 
 public interface IEstimationService
 {
-    public Estimation HandleUploadedFile(string userGuid, string directory, string fileName, string fileExtension, string displayName, IEnumerable<string> tags);
+    public void HandleUploadedFile(string userGuid, string directory, string fileName, string fileExtension, string displayName, IEnumerable<string> tags);
     public IEnumerable<Estimation> GetAllUserEstimations(string userGuid);
     public Stream? GetEstimationAttachment(string estimationid, AttachmentType attachmentType, string userGuid);
     public void DeleteEstimation(string estimationid, string userGuid);
